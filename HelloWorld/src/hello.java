be2781c70da555823a6732552d9e2423aade41cf
Ian Canaday <ian.m.canaday@gmail.com>
2013-11-14 20:01:39 -0600

    Initial Commit

---
+++ HelloWorld/src/hello.java
@@ -0,0 +1,14 @@
+
+public class hello {
+
+	public hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		System.out.println("Goodbye World");
+	}
+
+}
